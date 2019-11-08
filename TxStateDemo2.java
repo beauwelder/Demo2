c0e4c5c61d10c5078581d2410bbb4edd526ac339
Beau Welder <beauwelder@gmail.com>
2019-11-08 08:59:46 -0600

    Initial commit

---
+++ TxStateDemo2.java
@@ -0,0 +1,8 @@
+public class TxStateDemo2 {
+	
+	public static void main(String... args) {
+
+		String greeting = "This is the other way to link your code to a repo.";
+		System.out.println(greeting);
+	}
+}
